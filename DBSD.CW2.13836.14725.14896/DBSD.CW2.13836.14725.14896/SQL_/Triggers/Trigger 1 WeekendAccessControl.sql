c7277285cd63ed1212451a8fcc59ea044f9fbd3f
AzizbekStudent <00013836wiut@gmail.com>
2024-03-27 13:09:05 +0500

    Trigger for employee insert controlling
    Final Commit

--- DBSD.CW2.13836.14725.14896/DBSD.CW2.13836.14725.14896/SQL_/Triggers/Trigger 1 WeekendAccessControl.sql
+++ DBSD.CW2.13836.14725.14896/DBSD.CW2.13836.14725.14896/SQL_/Triggers/Trigger 1 WeekendAccessControl.sql
@@ -12,3 +12,18 @@ BEGIN
         ROLLBACK
     END
 END;
+
+
+go
+create or alter TRIGGER WeekendAccessControl_Employee
+ON Employee
+FOR INSERT, UPDATE, DELETE
+AS
+BEGIN
+    -- Check if it's Saturday (Day 7) or Sunday (Day 1)
+    IF DATEPART(dw, GETDATE()) IN (1, 7)
+    BEGIN
+        RAISERROR('Operations on the Employee table are not allowed during weekends.', 16, 1)
+        ROLLBACK
+    END
+END;
